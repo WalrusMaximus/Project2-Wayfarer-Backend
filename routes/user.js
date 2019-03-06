411ee098e527e9a83f64d43f5948c5d5303320a6
vagarioustoast <vagarioustoast@gmail.com>
2019-03-05 16:03:22 -0800

    adds token on login

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const controllers = require("../controllers");
 
 router.get("/", controllers.user.index);
+router.post("/login", controllers.user.login);
+router.post("/signup", controllers.user.signup);
+
+router.use((req, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    let verified = jwt.verify(req.token, "bWF0dGJyYW5kb25qb2VjaHJpc3RpbmE=");
+    console.log("Verified ", verifed);
+    req.userId = verified._id;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
 app.use("/users", userRoutes);
 app.use("/cities", cityRoutes);
 app.use("/posts", postRoutes);
