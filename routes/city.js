50a191317f9c855c1275d9946498d46c3b979884
vagarioustoast <vagarioustoast@gmail.com>
2019-03-09 16:42:32 -0800

    testing showOne city

--- routes/city.js
+++ routes/city.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const controllers = require("../controllers");
 
 router.get("/", controllers.city.index);
-
-
+router.get("/:id", controllers.city.showOne);
 
 module.exports = router;
